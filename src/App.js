edb5da6c91efd7fd13dcc2754d94eae390d2030c
akashm-2003 <akashmanna318@gmail.com>
2023-08-09 20:15:57 +0530

    starter-files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from './pages/Dashboard/Dashboard'
+import Login from './pages/Login/Login';
+import Professor from './pages/Professor/Professor';
+import ResearchPaper from './pages/ResearchPaper/ResearchPaper';
+import Profile from './pages/Profile/Profile';
+import Summarizer from './pages/Summarizer/Summarizer';
+import About from './pages/About/About';
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <><Dashboard /></>,
+    },
+    {
+      path: "/login",
+      element: <><Login /></>,
+    },
+    {
+      path: "/professor",
+      element: <><Professor /></>,
+    },
+    {
+      path: "/researchpaper",
+      element: <><ResearchPaper /></>,
+    },
+    {
+      path: "/profile",
+      element: <><Profile /></>,
+    },
+    {
+      path: "/summarizer",
+      element: <><Summarizer /></>,
+    },
+    {
+      path: "/about",
+      element: <><About /></>,
+    },
+  ]);
   return (
     <div className="App">
-      
+      <RouterProvider router={router} />
     </div>
   );
 }
