290a191838f70ae06a77a0ccbdd2e02defc22e42
Shreya Parkar <shreya.parkar.197@gmail.com>
2023-08-19 23:58:04 +0530

    shreya/Professor

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,9 +3,46 @@ import './Login.css'
 
 const Login = () => {
   return (
-    <>
-    </>
-  )
+    <div className="flex justify-center bg-gradient items-center h-screen ">
+      <div className="flex bg-bgColor rounded-lg p-4 bg-shadow flex-col">
+        <h1 className="text-[#fff] font-bold text-lg">Career Ahead</h1>
+        <hr className="mt-2 mb-2" />
+        {/* Log in Component */}
+        <h1 className="text-lg mt-2 text-hoverItem">Log in</h1>
+        <h2 className="text-sm text-hoverItem">Continue your research</h2>
+        <form className="mt-4">
+          <div className="flex flex-col p-1">
+            <label htmlFor="email" className="mt-2 mb-1 text-[#fff] text-lg">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="rounded-md text-lg"
+            />
+          </div>
+          <div className="flex flex-col p-1">
+            <label htmlFor="password" className="mt-2 mb-1 text-[#fff] text-lg">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="rounded-md text-lg"
+            />
+          </div>
+          <p className="flex flex-row p-1 justify-center">
+            OR
+          </p>
+          <button className='border border-width border-white rounded-lg p-2 text-[#fff]'>
+              Continue with Google 
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login
--- src/pages/Professor/Professor.jsx
+++ src/pages/Professor/Professor.jsx
@@ -4,6 +4,7 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 import ResearcherCard from "../../components/ResearcherCard/ResearcherCard";
 import Pagination from "../../components/Pagination";
 import Carousel from "../../components/Carousel/Carousel";
+import Sidebar from "../../components/Sidebar";
 
 // const slides = [
 //   <div className="w-full flex flex-row flex-shrink-0" >
@@ -19,73 +20,76 @@ import Carousel from "../../components/Carousel/Carousel";
 // ];
 
 const slides = [
-  <ResearcherCard classes={' flex-shrink-0 '} name={'Professor1'} />,
-  <ResearcherCard classes={' flex-shrink-0 '} name={'Professor2'} />,
-  <ResearcherCard classes={' flex-shrink-0 '} name={'Professor3'} />,
-  <ResearcherCard classes={' flex-shrink-0 '} name={'Professor4'} />,
-  <ResearcherCard classes={' flex-shrink-0 '} name={'Professor5'} />,
-  <ResearcherCard classes={' flex-shrink-0 '} name={'Professor6'} />,
-  <ResearcherCard classes={' flex-shrink-0 '} name={'Professor7'} />,
-  <ResearcherCard classes={' flex-shrink-0 '} name={'Professor8'} />,
-]
+  <ResearcherCard classes={" flex-shrink-0 "} name={"Professor1"} />,
+  <ResearcherCard classes={" flex-shrink-0 "} name={"Professor2"} />,
+  <ResearcherCard classes={" flex-shrink-0 "} name={"Professor3"} />,
+  <ResearcherCard classes={" flex-shrink-0 "} name={"Professor4"} />,
+  <ResearcherCard classes={" flex-shrink-0 "} name={"Professor5"} />,
+  <ResearcherCard classes={" flex-shrink-0 "} name={"Professor6"} />,
+  <ResearcherCard classes={" flex-shrink-0 "} name={"Professor7"} />,
+  <ResearcherCard classes={" flex-shrink-0 "} name={"Professor8"} />,
+];
 
-const Professor = ({ onSidebarHide }) => {
+const Professor = ({ showSidebar, onSidebarHide, onSetShowSidebar }) => {
   return (
-    <div className="flex w-full">
-      <div className="smallerDevice-container"></div>
-      <div className="main-container">
-        <SearchBar onSidebarHide={onSidebarHide} />
-        <div className="m-2 flex flex-col w-full">
-          <div className=" pt-2 pb-2 flex w-full ">
-            <div className="flex flex-row justify-between w-full">
-              <h1 className="text-2xl font-bold text-white ">
-                Professors in your selected domain
-              </h1>
-              <div className=" text-[#676767] hover:text-[#a1a1a0] flex flex-row right-0">
-                <p className="mr-2 sm:">See all</p>
-                <AiOutlineArrowRight className="pt-2 pr-2 text-xl" />
+    <>
+      <Sidebar showSidebar={showSidebar} onSetShowSidebar={onSetShowSidebar} />
+      <div className="flex w-full">
+        <div className="smallerDevice-container"></div>
+        <div className="main-container">
+          <SearchBar onSetShowSidebar={onSetShowSidebar} />
+          <div className="m-2 flex flex-col w-full">
+            <div className=" pt-2 pb-2 flex w-full ">
+              <div className="flex flex-row justify-between w-full">
+                <h1 className="text-2xl font-bold text-white ">
+                  Professors in your selected domain
+                </h1>
+                <div className=" text-[#676767] hover:text-[#a1a1a0] flex flex-row right-0">
+                  <p className="mr-2 sm:">See all</p>
+                  <AiOutlineArrowRight className="pt-2 pr-2 text-xl" />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="flex flex-col w-full">
-            {/* <div className="flex flex-row w-full justify-between items-center">
+            <div className="flex flex-col w-full">
+              {/* <div className="flex flex-row w-full justify-between items-center">
               classes={"w-full lg:w-1/3 md:w-1/2 sm:w-full"}
               <ResearcherCard />
               <ResearcherCard />
               <ResearcherCard />
             </div> */}
-            <div className="w-full flex flex-row flex-shrink-0 justify-between items-center" >
-              <Carousel slides={slides}></Carousel>
-            </div>
-          </div>
-        </div>
-
-        <div className="m-2 flex flex-col w-full">
-          <div className=" pt-2 pb-2 flex w-full ">
-            <div className="flex flex-row justify-between w-full">
-              <h1 className="text-2xl font-bold text-white ">
-                Professors you may know
-              </h1>
-              <div className=" text-[#676767] hover:text-[#a1a1a0] flex flex-row right-0">
-                <p className="mr-2">See all</p>
-                <AiOutlineArrowRight className="pt-2 pr-2 text-xl" />
+              <div className="w-full flex flex-row flex-shrink-0 justify-between items-center">
+                <Carousel slides={slides}></Carousel>
               </div>
             </div>
           </div>
-          <div className="flex flex-col w-full">
-            <div className="flex flex-row w-full justify-between items-center">
-              {/* classes={"w-full lg:w-1/3 md:w-1/2 sm:w-full"} */}
-              <ResearcherCard />
-              <ResearcherCard />
-              <ResearcherCard />
+
+          <div className="m-2 flex flex-col w-full">
+            <div className=" pt-2 pb-2 flex w-full ">
+              <div className="flex flex-row justify-between w-full">
+                <h1 className="text-2xl font-bold text-white ">
+                  Professors you may know
+                </h1>
+                <div className=" text-[#676767] hover:text-[#a1a1a0] flex flex-row right-0">
+                  <p className="mr-2">See all</p>
+                  <AiOutlineArrowRight className="pt-2 pr-2 text-xl" />
+                </div>
+              </div>
             </div>
-            {/* DO NOTE : while requesting data request 3 professors per page for desktop size and 1 for ipad and phone size  */}
-            {/* currently pages are static for dynamic fetching and associating it with pages refer https://www.youtube.com/watch?v=GWsS2eYFZOk&t=16s */}
-            <Pagination />
+            <div className="flex flex-col w-full">
+              <div className="flex flex-row w-full justify-between items-center">
+                {/* classes={"w-full lg:w-1/3 md:w-1/2 sm:w-full"} */}
+                <ResearcherCard />
+                <ResearcherCard />
+                <ResearcherCard />
+              </div>
+              {/* DO NOTE : while requesting data request 3 professors per page for desktop size and 1 for ipad and phone size  */}
+              {/* currently pages are static for dynamic fetching and associating it with pages refer https://www.youtube.com/watch?v=GWsS2eYFZOk&t=16s */}
+              <Pagination />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
