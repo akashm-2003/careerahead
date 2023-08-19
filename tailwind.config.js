d8f288cccd97fa1a01ce752b5162c1f53a64bde7
akashm-2003 <akashmanna318@gmail.com>
2023-08-19 21:53:12 +0530

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,6 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
-    colors: {
-      'heading': "#0004FF",
-      'subHeading': "#6062FF",
-      'darkHeading': '#0002B7',
-      'unHighlightedText': "#676767",
-      'highlightedText': "#fff",
-      'bgColor': "#1e1e1e",
-      'bgCard': "#171717",
-      'bgIcon': '#2d2d2d',
-      'hoverItem': '#a1a0a0',
-      'selectedItem': '#fff'
-    },
-  },
+
 
 }
